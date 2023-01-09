0b1931019acef5263ac0446db5171d058e24f12f
Fred-H-McBurnett <fhmtechnology@gmail.com>
2023-01-09 10:41:05 -0600

    completed ruby challenge, interview, and 2 of the three code challenges.  Struggled with the first one of those, because I could not get the function to remove the initial value of the array.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -27,8 +27,35 @@ const colors2 = [
 ]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+// JEST TEST
+// Describe funtion("colorsShuffled"), () => {
+//  it ([returns a shuffled array with the first coler deleted] from colors1)
+// it ([returns a shuffled array with the first color deleted from colors2])
+// expected output: ["yellow", "blue", "pink", "green"]
+// expected output: ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+//})
+
 // b) Create the function that makes the test pass.
 
+// Psuedo-code
+// 1. Create function(colorsShuffled)
+// 2. Using the .shift method, remove the first item from each array
+// 3. Using a for loop, create function var array1 to store the randomized values of the remaining elements of the array
+// 4. Console log the shuffled array
+
+  const colorsShuffled = (array) => {
+  let array1 = array.shift(0)
+  // for (arrayX = 0; i < arrayX.length; i++) {
+  //   var j = Math.floor(Math.random() * (i - 1));
+  //   var temp = array[i];
+  //   array[i] = array[j];
+  //   array[j] = temp;
+  // }
+}
+console.log(colors1)
+
+//  I could not get the .shift method to work in this problem, which kept me from getting the second part of the problem.  I could only get it to reverse the order of the elements of the array without removing the first one before reversal. My intent was to use .shift to remove the first element of the array, and then use a for loop to shuffle the remaining elements.
+ 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
@@ -38,7 +65,31 @@ const votes1 = { upVotes: 13, downVotes: 2 }
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
+// JEST Test
+// describe finalTally
+// it takes in the two values of votes1, determines which is the larger value, and performs subtraction to get the finalTally
+// expected output: 11
+// it takes in the values of votes 2, compares the two values, and perform addition to get the finalTally
+// expected output: -31
+
 // b) Create the function that makes the test pass.
+
+// Psuedo-code
+// 1. Create function finalTally
+// 2. finalTally takes the two values of each constant
+// 3. use comparison operator to determine which value is larger
+// 4. use addition to obtain finalTally of each constant provided
+// 5. Consol.log the results for both of the constants
+
+const finalTally = (upVotes, downVotes) => {
+  if (upVotes > downVotes) {
+    return(upVotes - downVotes)
+    } else {return(-(downVotes) + upVotes)
+    }
+  }
+
+  console.log(finalTally(13, 2))
+  console.log(finalTally(2, 33))
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
@@ -48,4 +99,37 @@ const dataArray1 = ["array", "object", "number", "string", "Boolean"]
 const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
 
+// describe combined_Array
+// it(takes in the values from dataArray1), storing them into a new array
+// it (takes in the values from dataArray2 and compares them to the values already in the new array, removing any duplicated values)
+// Expectede output : ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+
 // b) Create the function that makes the test pass.
+
+// Psuedo-code
+// 1. Create a function that takes in the elements of dataArray1 and loops through those elements to get the initial value of array3
+// 2. Using a nested loop, take in values of dataArray2 and loops through those elements, splicing the non-duplicated elements into array3
+// 3. Console logs the combined arrays of dataArray1 and dataArray2, with the duplicate elements removed
+
+Array.prototype.unique = function() {
+  var a = this.concat();
+  // First loop takes in the values in dataArray 1
+  for(var i=0; i<a.length; ++i) {
+      // Second loop takes in values from dataArray2 and compares them to dataArray1, splicing the new values into the array a
+      for(var j=i+1; j<a.length; ++j) {
+        // if value in dataArray2 is a duplicate of value from dataArray2, remove the duplicate values
+        if(a[i] === a[j])
+              a.splice(j--, 1);
+      }
+  }
+
+  return a;
+};
+// Merges both arrays and gets unique items
+var dataArray3 = dataArray1.concat(dataArray2).unique(); 
+console.log(dataArray3)
+
+// After completing the assignment, I looked for ways in which to refactor my code.  I could not think of any method that would allow me to do so, but in my research I found the below code snippet that took all of the code and reduced it to one line (the console.log added at the end was to see the output in order to verify the code works.) 
+
+// const result = [...new Set([...dataArray1, ...dataArray2])]
+// console.log(result)
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -3,6 +3,16 @@
 
 # --------------------1) Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.
 
+# Psuedo-code
+# 1.  Take given input values of num1, num2, and num3
+# 2.  Define the function is_even
+# 3.  Uses the modulo method on each of the values given to determine its remainder
+# 4.  If the modulo is 1, output that the given value is odd
+# 5.  If the modulo is 0, output that the given value is even
+# 6. Test each of the following values given to verify correct output
+
+
+
 num1 = 7
 # Expected output: '7 is odd'
 num2 = 42
@@ -10,6 +20,19 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
+def is_even(num)
+    if (num % 2 == 0)
+        p "#{num} is even."
+    else p "#{num} is odd."    
+    end    
+end
+
+is_even(7)
+# output: '7 is odd.'
+is_even(12)
+# output: '12 is even.'
+is_even(221)
+# output: '221 is odd.'
 
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
@@ -22,9 +45,31 @@ beatles_album2 = 'Sgt Pepper'
 beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+# Psuedo-code
+# 1. Create a method called vowels_out
+# 2. Take given input values of beatles_album1, beatles_album2, and beatles_album3
+# 3. Use the .sub method to remove all of the vowels in each given string
+# 4. Test each of the given values to verify correct output, with vowels removed from each
 
+# def vowels_out(album)
+#     album.sub(/[aeiou]/, '')
+#         p "#{album}"
+
+# This is my favorite solution.
+def vowels_out(string)
+    result = string.gsub(/[aeiou]/i, '')
+    p "#{result}"
+end
+
+vowels_out(beatles_album1)
+# output: "Rbbr Sl"
+vowels_out(beatles_album2)
+# output: "Sgt Pppr"
+vowels_out(beatles_album3)
+# output: "bby Rd"
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
+
 
 palindrome_tester1 = 'Racecar'
 # Expected output: 'Racecar is a palindrome'
@@ -32,3 +77,26 @@ palindrome_tester2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_tester3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+# Psuedo-code 
+# 1. Create a method called pal_test
+# 2. Using the given values for palindrome_tester1, palindrome_tester2, and palindrome_tester3
+# 3. Using the method pal_test, test if the values given are palindromes
+# 4. If the word is a palingdrome, return (value) is a palindrome.
+# 5. If the word is not a palingdrome, return (value) is not a palindrome.
+# 6. Test each of the given values to verify status
+
+
+def pal_test(string)
+    if string.upcase == string.upcase.reverse
+        p "#{string} is a palindrome."
+    else p "#{string} is not a palindrome."
+    end    
+end
+
+pal_test(palindrome_tester1)
+# output: "Racecar is a palindrome."
+pal_test(palindrome_tester2)
+# output: "LEARN is not a palindrome."
+pal_test(palindrome_tester3)
+# output: "Rotator is a palindrome."
